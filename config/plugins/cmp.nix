866cef46ae095efe7a09fa7045f2b0b4c32b8d98
Jacopown <jacopown@ubuntuMachine>
2024-11-24 23:59:58 +0100

    made the window look nicer

--- config/plugins/cmp.nix
+++ config/plugins/cmp.nix
@@ -2,14 +2,25 @@
 #TODO add colors and icons
 {
   plugins.luasnip.enable = true; #Snippet engine
+  plugins.lspkind = {
+    enable = true;
+    preset = "default";
+    cmp.menu = {
+      buffer = "[Buffer]";
+      luasnip = "[LuaSnip]";
+      nvim_lsp = "[LSP]";
+      path = "[Path]";
+    };
+  };
   plugins.cmp = {
     enable = true;
     autoEnableSources = true;
     settings = { 
       sources = [
-        { name = "path"; }
-        { name = "luasnip"; } #Adapter for luasnip snippet engine to cmp
         { name = "buffer"; }
+        { name = "luasnip"; } #Adapter for luasnip snippet engine to cmp
+        { name = "nvim_lsp"; }
+        { name = "path"; }
       ];
       view.entries = {
         name = "custom";
@@ -32,6 +43,9 @@
           '';
         "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
         "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+      };
+      experimental = {
+        ghost_text = true;
       };
     };
   };
