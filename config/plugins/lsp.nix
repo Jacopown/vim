9347db7edc9323f7a18d8d287fb066cefc9afca5
Jacopown <jacopown@ubuntuMachine>
2024-11-24 23:59:32 +0100

    added some keymaps

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -1,10 +1,24 @@
 { lib, inputs, pkgs, config, ...}:
 {
-  plugins.lsp = {
+plugins.lsp = {
     enable = true;
     inlayHints = true;
     servers = {
       nil_ls.enable = true;
     };
+    keymaps.lspBuf = {
+      gD = "declaration";  
+      gd = "definition";
+      gi = "implementation";
+      gr = "references";
+    };
   };
+  # plugins.conform-nvim = {
+  #   enable = true;
+  #   settings = {
+  #     formatters_by_ft = {
+  #       nix = [ "nil_ls" ];
+  #     };
+  #   };
+  # };
 }
