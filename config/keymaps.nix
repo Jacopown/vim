898a0e0df47a282c4bed2ed44f53bf361393717d
Jacopown <jacopown@NixOS>
2024-12-01 10:33:20 +0100

    added some new keybinds

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -1,5 +1,10 @@
-{ lib, inputs, pkgs, config, ...}:
 {
+  lib,
+  inputs,
+  pkgs,
+  config,
+  ...
+}: {
   globals = {
     mapleader = " ";
   };
@@ -46,6 +51,69 @@
       options = {
         silent = true;
         desc = "Move selected line down";
+      };
+    }
+    {
+      mode = "n";
+      key = "<C-h>";
+      action = "<C-w>h";
+      options = {
+        silent = true;
+        desc = "Move window focus to the right";
+      };
+    }
+    {
+      mode = "n";
+      key = "<C-l>";
+      action = "<C-w>l";
+      options = {
+        silent = true;
+        desc = "Move window focus to the left";
+      };
+    }
+    {
+      mode = "n";
+      key = "<C-k>";
+      action = "<C-w>k";
+      options = {
+        silent = true;
+        desc = "Move window focus up";
+      };
+    }
+    {
+      mode = "n";
+      key = "<C-j>";
+      action = "<C-w>j";
+      options = {
+        silent = true;
+        desc = "Move window focus down";
+      };
+    }
+    {
+      mode = "v";
+      key = "<";
+      action = ":<gv";
+      options = {
+        silent = true;
+        desc = "Keep selection when changing indentation to selection";
+      };
+    }
+    {
+      mode = "v";
+      key = ">";
+      action = ":>gv";
+      options = {
+        silent = true;
+        desc = "Keep selection when changing indentation to selection";
+      };
+    }
+    {
+      mode = "v";
+      key = "p";
+      action = ''"_dP'';
+      options = {
+        silent = true;
+        desc = "Paste to substitute a selection without rewriting the copied text with the substututed one";
       };
     }
   ];
